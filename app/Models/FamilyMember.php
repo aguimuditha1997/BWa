56618fee14b8f24a2b856dbea6cc09216cbb222d
aguimuditha1997 <aa.agui97@gmail.com>
2025-04-28 14:18:58 +0800

    secound commit

---
+++ app/Models/FamilyMember.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FamilyMember extends Model
+{
+    use SoftDeletes, UUID;
+    protected $fillable = [
+        'head_of_family_id',
+        'user_id',
+        'profile_picture',
+        'identity_number',
+        'gender',
+        'date_of_birth',
+        'phone_number',
+        'occupation',
+        'marital_status',
+        'relation'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
